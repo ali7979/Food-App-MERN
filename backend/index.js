4dc0d5a79417033d7de3d7cf5b333cb5d83b7847
Zoheb Alam <zohebzob@gmail.com>
2023-05-19 11:06:15 +0530

    CORS ERROR

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const connectdb = require('./db/Connectdb')
+const cors = require("cors");
 const app = express()
 
 const dotenv=require('dotenv').config();
 
-app.use((req, res, next)=> {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 
-    'Origin,X-Requested-With,Content-Type,Accept');
+
+app.use(cors())
+
+// app.use((req, res, next)=> {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 
+//     'Origin,X-Requested-With,Content-Type,Accept');
    
-    next();
-});
+//     next();
+// });
 
 app.use(express.json())
 
